4b49f1954725991610a4566e28f7462023c744ca
YadiSuarez <151183232+YadiSuarez@users.noreply.github.com>
2025-12-23 21:06:35 -0500

    stats

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,4 @@ Route::post('/denuncias', [DenunciaController::class, 'store']);
 Route::put('/cambiar-estado/{id}', [DenunciaController::class, 'updateStatus']);
 Route::get('/denuncias/consultar/{codigo}', [DenunciaController::class, 'showByCode']);
 Route::get('/denuncias/buscar', [DenunciaController::class, 'search']);
+Route::get('/estadisticas-publicas', [DenunciaController::class, 'publicStats']);
